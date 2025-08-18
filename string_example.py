a7f16072e0add1703bfedf1d46b09de98bbdfb8d
VUHOANGTHE2610 <hoangthevu8910@gmail.com>
2025-08-18 21:36:34 +0700

    Initial commit

---
+++ string_example.py
@@ -0,0 +1,6 @@
+tu_than = "Tu thân"
+te_gia = "tề gia"
+tri_quoc = "trị quốc"
+binh_Thien_ha = "bình thiên hạ"
+
+
---
+++ varieble.py
@@ -0,0 +1,9 @@
+author_name = "Hoàng Thế Vũ"
+
+print("Gạo đem vào dã bao đau đơn")
+print("Gạo dã xong rồi trắng tựa bông")
+print("Sống ở trên đời người cũng vậy")
+print("Gian nan rèn luyện mới thành công")
+
+author_name: str = "Hồ Chí Minh"
+print(f"               Chủ tịch: {author_name}")
